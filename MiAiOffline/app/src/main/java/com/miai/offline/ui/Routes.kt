305d5a0dd6319fed5c4468330191bc7731e3da24
muaaziqbalvip <muaaziqbal.vip@gmail.com>
2026-06-28 18:47:08 +0500

    Delete MiAiOffline directory

--- MiAiOffline/app/src/main/java/com/miai/offline/ui/Routes.kt
+++
@@ -1,9 +0,0 @@
-package com.miai.offline.ui
-
-object Routes {
-    const val HOME = "home"
-    const val MODELS = "models"
-    const val CHAT = "chat/{modelId}"
-
-    fun chatRoute(modelId: String) = "chat/$modelId"
-}
